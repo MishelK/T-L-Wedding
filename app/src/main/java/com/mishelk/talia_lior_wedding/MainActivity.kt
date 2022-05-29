0ff6ca41d2e19ae9a05e458e4cd2b2f5c41b0664
“MishelK” <84840131+mishel-k@users.noreply.github.com>
2022-05-29 23:14:18 +0300

    Init project

---
+++ app/src/main/java/com/mishelk/talia_lior_wedding/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mishelk.talia_lior_wedding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
