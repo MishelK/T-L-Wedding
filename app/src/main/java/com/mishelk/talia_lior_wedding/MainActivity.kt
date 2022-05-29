89b8289840bbd28e48101f7dd3053798d39f32b7
“MishelK” <84840131+mishel-k@users.noreply.github.com>
2022-05-29 23:54:04 +0300

    Implemented android lifecycle extensions, setting main_page as selected by default

--- app/src/main/java/com/mishelk/talia_lior_wedding/MainActivity.kt
+++ app/src/main/java/com/mishelk/talia_lior_wedding/MainActivity.kt
@@ -2,10 +2,14 @@ package com.mishelk.talia_lior_wedding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Setting main page as the selected navigation option
+        bottom_navigation.selectedItemId = R.id.main_page
     }
 }
