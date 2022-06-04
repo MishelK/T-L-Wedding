b6ddddbdae39241a4f69ae99cebdf7bdc4ebf4cc
Mishel Kvit <65822657+MishelK@users.noreply.github.com>
2022-06-04 20:22:04 +0300

    Added fragments and hebrew texts

--- app/src/main/java/com/mishelk/talia_lior_wedding/MainActivity.kt
+++ app/src/main/java/com/mishelk/talia_lior_wedding/MainActivity.kt
@@ -2,6 +2,11 @@ package com.mishelk.talia_lior_wedding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.navigation.NavigationBarView
+import com.mishelk.talia_lior_wedding.fragments.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,7 +14,39 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottomNavigation.setOnItemSelectedListener(object : NavigationBarView.OnItemSelectedListener {
+            override fun onNavigationItemSelected(item: MenuItem): Boolean {
+                when (item.itemId) {
+                    R.id.main_page -> {
+                        switchToFragment(MainFragment())
+                        return true
+                    }
+                    R.id.greetings_page -> {
+                        switchToFragment(GreetingsFragment())
+                        return true
+                    }
+                    R.id.presents_page -> {
+                        switchToFragment(PresentsFragment())
+                        return true
+                    }
+                    R.id.riddles_page -> {
+                        switchToFragment(RiddlesFragment())
+                        return true
+                    }
+                    R.id.settings_page -> {
+                        switchToFragment(SettingsFragment())
+                        return true
+                    }
+                }
+                return false
+            }
+        })
         // Setting main page as the selected navigation option
-        bottom_navigation.selectedItemId = R.id.main_page
+        bottomNavigation.selectedItemId = R.id.main_page
+    }
+
+    private fun switchToFragment(fragment: Fragment) {
+        val manager: FragmentManager = supportFragmentManager
+        manager.beginTransaction().replace(R.id.contentFragment, fragment).commit()
     }
 }
